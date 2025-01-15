7d514010cfd487afd6c59bccb96ef2ac378fef3c
Yahel Maydani <yahel.maydani@idf.il>
2025-01-15 09:03:13 +0200

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
+import { Card, CardContent, CardTitle } from "~/components/ui/card";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import { differenceInDays } from "date-fns";
+import { cn } from "~/lib/utils";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,5 +10,35 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  function checkIfPassed(index: number) {
+    return differenceInDays(new Date(2025, 3, 30), new Date()) < index;
+  }
+
+  return (
+    <div className="flex justify-center">
+      <Card>
+        <h1 className="text-4xl text-center m-2"> 100 Board</h1>
+        <CardContent className="grid grid-rows-10 grid-cols-10 gap-2">
+          {Array(100)
+            .fill(null)
+            .map((v, index) => (
+              <div
+                className={cn(
+                  "aspect-square h-20 w-20 bg-red-500 text-white text-center grid place-items-center p-2",
+                  {
+                    "bg-green-500 ": checkIfPassed(index),
+                  }
+                )}
+              >
+                <div>
+                  <p className="text-xl">{index + 1}</p>
+                  <p className="capitalize text-sm">days to freedom</p>
+                </div>
+              </div>
+            ))
+            .reverse()}
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
