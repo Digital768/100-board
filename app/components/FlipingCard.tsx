19f6c4d5ee8e214e04e7ae94673a68822b58d6fc
Uriel Y. Faran <xd.ufaran@idf.il>
2025-04-02 09:04:25 +0300

    add flipping card

---
+++ app/components/FlipingCard.tsx
@@ -0,0 +1,44 @@
+import { subDays } from "date-fns";
+import { cn } from "~/lib/utils";
+import { checkIfPassed, theDate } from "~/routes/home";
+
+function FlipingCard({ index }: { index: number }) {
+  const isPassed = checkIfPassed(index);
+  return (
+    <div className="mx-auto flex justify-center object-center">
+      <div className="group h-20 w-20 [perspective:1000px]">
+        <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+          {/* Front Face */}
+          <div
+            className={cn(
+              "absolute inset-0 rounded-xl [backface-visibility:hidden] aspect-square h-20 w-20 bg-red-500 text-white text-center grid place-items-center p-2",
+              {
+                "bg-green-500 ": isPassed,
+              }
+            )}
+          >
+            <div>
+              <p className="text-xl">{index + 1}</p>
+              <p className="capitalize text-sm">days to freedom</p>
+            </div>
+          </div>
+          {/* Back Face */}
+          <div
+            className={cn(
+              "absolute inset-0 h-full w-full bg-red-500 text-center grid place-items-center p-2 rounded-xl text-white [transform:rotateY(180deg)] [backface-visibility:hidden]",
+              {
+                "bg-green-500": isPassed,
+              }
+            )}
+          >
+            <p className="flex flex-row text-s text-center">
+              {new Date(subDays(theDate, index + 1)).toLocaleDateString()}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FlipingCard;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,41 +1,30 @@
-import { Card, CardContent, CardTitle } from "~/components/ui/card";
-import type { Route } from "./+types/home";
 import { differenceInDays } from "date-fns";
-import { cn } from "~/lib/utils";
+import FlipingCard from "~/components/FlipingCard";
+import { Card, CardContent } from "~/components/ui/card";
+import type { Route } from "./+types/home";
+
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "100 Board" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export default function Home() {
-  function checkIfPassed(index: number) {
-    return differenceInDays(new Date(2025, 3, 30), new Date()) < index;
-  }
+export function checkIfPassed(index: number) {
+  return differenceInDays(theDate, new Date()) <= index;
+}
 
+export const theDate = new Date(2025, 3, 30);
+
+export default function Home() {
   return (
-    <div className="flex justify-center">
-      <Card>
-        <h1 className="text-4xl text-center m-2"> 100 Board</h1>
+    <div className="flex justify-center bg-[url(/board.jpg)]">
+      <Card className="bg-inherit backdrop-blur-sm bg-opacity-5">
+        <h1 className="text-5xl text-center p-2 text-white"> 100 Board</h1>
         <CardContent className="grid grid-rows-10 grid-cols-10 gap-2">
           {Array(100)
             .fill(null)
-            .map((v, index) => (
-              <div
-                className={cn(
-                  "aspect-square h-20 w-20 bg-red-500 text-white text-center grid place-items-center p-2",
-                  {
-                    "bg-green-500 ": checkIfPassed(index),
-                  }
-                )}
-              >
-                <div>
-                  <p className="text-xl">{index + 1}</p>
-                  <p className="capitalize text-sm">days to freedom</p>
-                </div>
-              </div>
-            ))
+            .map((v, index) => <FlipingCard index={index} key={v} />)
             .reverse()}
         </CardContent>
       </Card>
